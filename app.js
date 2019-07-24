db3a42764c86c33be36f87986cc158e2cd9cf9da
Brian Ahn <emfoa23@gmail.com>
2019-07-24 11:39:40 +0900

    on going

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
-app.set("view engine", "mustache");
+// app.set("view engine", "mustache");
 
 app.get("/", function(req, res){
 	res.render("index");
