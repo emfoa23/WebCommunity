8f8fef49e3e57ce267895e390d936d84cc6578df
Brian Ahn <emfoa23@gmail.com>
2019-07-24 19:00:31 +0900

    서버에 올려놓은거 pug, sass 쓰는 방식으로 다시 만듦

--- app.js
+++ app.js
@@ -24,7 +24,10 @@ app.use(sassMiddleware({
   indentedSyntax: true, // true = .sass and false = .scss
   sourceMap: true
 }));
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/node_modules/bootstrap/dist')));
+app.use(express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
