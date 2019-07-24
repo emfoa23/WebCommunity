6880fc92f2b65d3a7029ece83b6c64cec7f20a50
Brian Ahn <emfoa23@gmail.com>
2019-07-24 11:55:53 +0900

    fix

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const port = 4000;
 app.set("view engine", "ejs");
 app.engine("html", require("ejs").renderFile);
 
+app.use(express.static('public'));
+
 app.get("/", function(req, res){
 	res.render("index.html");
 });
