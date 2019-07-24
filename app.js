c457cf83bea61c3c136113ffa5e4d3517855f4fe
Brian Ahn <emfoa23@gmail.com>
2019-07-24 01:09:44 +0000

    server now

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 4000;
 
 app.get("/", function(req, res){
-	res.send("Hello World!");
+	res.send("Hello Node!");
 });
 
-app.listen(port, function(){
-	console.log("app listening");
+app.listen(port, '0.0.0.0', function(err){
+	if (err) {
+		console.log("error!");
+	}
+	else {
+		console.log("app listening");
+	}
 });
